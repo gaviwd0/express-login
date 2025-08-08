0180594791ca65bf6987a5a2ef62bb1d5babeca4
Gaviwd0 <gaviwd@gmail.com>
2025-08-08 18:04:53 -0300

    validacion email existente al registrarse

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,7 +6,10 @@ dotenv.config();
 
 //register func
 export const register = async (req, res) => {
+    const existEmail= await User.findOne({ where: { email: req.body.email } });
+    
     try {
+        if(existEmail) return res.status(400).json({ message: 'Email already exists on system' }); 
         const { name, email, password, role } = req.body;
         const user = await User.create({ name, email, password, role });
         res.status(201).json({ message: 'User created successfully', userId: user.id });
